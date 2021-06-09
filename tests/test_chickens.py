48ef7f5ef77ed01da171f9047c220f7f6fc7340f
Alex-AMC <a.a.moldovan@gmail.com>
2021-06-09 21:29:37 +0100

    Initial Commit of Chickens and their Lovely tests

---
+++ tests/test_chickens.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+import chickens
+
+
+class TestChickens(TestCase):
+
+    def setUp(self) -> None:
+        self.chicken = chickens.Chicken(name='Daisy', age=1)
+
+    def test_adding_works(self):
+        """ Adding an egg to the chicken works"""
+        self.chicken.total_eggs = 0
+        self.chicken.add_egg_to_total(2)
+        self.assertEqual(self.chicken.total_eggs, 2)
+
+    def test_age_works(self):
+        """ I can see the age"""
+        self.assertEqual(self.chicken.age, 1)
